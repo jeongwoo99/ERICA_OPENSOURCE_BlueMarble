e6b34be2b7f82638cf13293bf87a1a7768d3e648
kim-dong-woo <dongwoo12048@gmail.com>
2019-12-14 02:18:17 +0900

    플레이 할 사람수 한글로 & 차례넘기는 함수

--- BlueMarble.py
+++ BlueMarble.py
@@ -1,10 +1,24 @@
-def count_player():#input how many players
+def count_player():#플레이 할 사람수 입력
     while True:
         try:
-            x = int(input("How many player?"))
+            x = int(input("몇명?"))
             while x not in {2,3,4}:
-                x = int(input("How many player?"))
+                x = int(input("몇명?"))
             break
         except ValueError:
             continue
     return x
+
+def change_turn():#차례 넘기기
+    while True:
+        try:
+            x = input("차례를 넘기시려면 y를 입력해주세요.")
+            while x != 'y':
+                x = input("차례를 넘기시려면 y를 입력해주세요.")
+            break
+        except ValueError:
+            continue
+    if x == 'y':
+        return True
+    else:
+        return False
